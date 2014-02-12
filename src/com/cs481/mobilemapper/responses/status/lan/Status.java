af80889c189d4ac13dd83387fe25e3c83d1ba5a7
Mgamerz <mgamerz2@gmail.com>
2014-02-12 10:29:47 -0700

    Fixed link_flags to arraylist<string>

--- src/com/cs481/mobilemapper/responses/status/lan/Status.java
+++ src/com/cs481/mobilemapper/responses/status/lan/Status.java
@@ -2,21 +2,20 @@
 package com.cs481.mobilemapper.responses.status.lan;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Status{
 	@JsonProperty("link_flags")
-   	private ArrayList<link_flag> link_flags;
+   	private ArrayList<String> link_flags;
    	
    	@JsonProperty("link_state")
    	private String link_state;
 
- 	public ArrayList<link_flag> getLink_flags(){
+ 	public ArrayList<String> getLink_flags(){
 		return this.link_flags;
 	}
-	public void setLink_flags(ArrayList<link_flag> link_flags){
+	public void setLink_flags(ArrayList<String> link_flags){
 		this.link_flags = link_flags;
 	}
  	public String getLink_state(){
