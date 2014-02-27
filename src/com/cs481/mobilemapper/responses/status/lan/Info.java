8fa2cc1fbb4a2ec70c3bfe2e92b9babdde8b0bb1
swright1021 <seanwright@u.boisestate.edu>
2014-02-26 20:54:36 -0700

    Created Info class under responses/lan.

---
+++ src/com/cs481/mobilemapper/responses/status/lan/Info.java
@@ -0,0 +1,107 @@
+package com.cs481.mobilemapper.responses.status.lan;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Info {
+	
+	@JsonProperty("uid")
+	private Number uid;
+	
+	@JsonProperty("ip6_addresses")
+	private List ip6_addresses;
+	
+	@JsonProperty("ip_address")
+	private Number ip_address;
+	
+	@JsonProperty("ip_addresses")
+	private List ip_addresses;
+	
+	@JsonProperty("hostname")
+	private String hostname;
+	
+	@JsonProperty("broadcast")
+	private String broadcast;
+	
+	@JsonProperty("netmask")
+	private String netmask;
+	
+	@JsonProperty("type")
+	private String type;
+	
+	@JsonProperty("name")
+	private String name;
+	
+	public Number getUid() {
+		return uid;
+	}
+
+	public void setUid(Number uid) {
+		this.uid = uid;
+	}
+
+	public List getIp6_addresses() {
+		return ip6_addresses;
+	}
+
+	public void setIp6_addresses(List ip6_addresses) {
+		this.ip6_addresses = ip6_addresses;
+	}
+
+	public Number getIp_address() {
+		return ip_address;
+	}
+
+	public void setIp_address(Number ip_address) {
+		this.ip_address = ip_address;
+	}
+
+	public List getIp_addresses() {
+		return ip_addresses;
+	}
+
+	public void setIp_addresses(List ip_addresses) {
+		this.ip_addresses = ip_addresses;
+	}
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+
+	public String getBroadcast() {
+		return broadcast;
+	}
+
+	public void setBroadcast(String broadcast) {
+		this.broadcast = broadcast;
+	}
+
+	public String getNetmask() {
+		return netmask;
+	}
+
+	public void setNetmask(String netmask) {
+		this.netmask = netmask;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
