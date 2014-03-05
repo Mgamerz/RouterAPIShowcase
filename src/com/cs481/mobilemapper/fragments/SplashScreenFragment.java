6c995da5c09d84869190e919ac4fc97453c3c620
Sasa Rkman <duskorsn@gmail.com>
2014-03-04 19:57:23 -0700

    Splash screen bugfix. There's still an issue where landscape mode makes it look ugly.

--- src/com/cs481/mobilemapper/fragments/SplashScreenFragment.java
+++ src/com/cs481/mobilemapper/fragments/SplashScreenFragment.java
@@ -1,13 +1,11 @@
 package com.cs481.mobilemapper.fragments;
 
 import android.content.SharedPreferences;
-import android.graphics.Point;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,7 +13,6 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
-import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -45,16 +42,17 @@ public class SplashScreenFragment extends Fragment {
 	Animation fadeInAnim;
 	Animation fadeInAndOutAnim;
 
-	TranslateAnimation moveUp;
-	TranslateAnimation moveDown;
+/*	TranslateAnimation moveUp;
+	TranslateAnimation moveDown;*/
+	Animation moveUp;
+	Animation moveDown;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// Inflate the layout for this fragment
-		View view = inflater.inflate(R.layout.fragment_splashscreen, container,
-				false);
+		View view = inflater.inflate(R.layout.fragment_splashscreen, container, false);
 
 		ecm_button = (ImageView) view.findViewById(R.id.button_ecm);
 		local_button = (ImageView) view.findViewById(R.id.button_local);
@@ -74,25 +72,20 @@ public class SplashScreenFragment extends Fragment {
 		indicator[5] = (TextView) view.findViewById(R.id.down3);
 
 		// Used for retrieving user preferences
-		final SharedPreferences prefs = PreferenceManager
-				.getDefaultSharedPreferences(getActivity()
-						.getApplicationContext());
+		final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
 
 		ecm_button.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg) {
 				// Display the preferred connection dialog
 				if (!prefs.getBoolean("prefs_connection_dontAskAgain", false)) {
-					PreferredConnectionDialog rcFragment = PreferredConnectionDialog
-							.newInstance("ECM");
-					rcFragment.show(getActivity().getSupportFragmentManager(),
-							"PreferredConnection");
+					PreferredConnectionDialog rcFragment = PreferredConnectionDialog.newInstance("ECM");
+					rcFragment.show(getActivity().getSupportFragmentManager(),"PreferredConnection");
 				}
 
 				// Load the ECM login fragment
 				ECMLoginFragment ecmLoginFragment = new ECMLoginFragment();
-				FragmentTransaction transaction = getActivity()
-						.getSupportFragmentManager().beginTransaction();
+				FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
 				transaction.replace(R.id.login_fragment, ecmLoginFragment);
 				transaction.commit();
 			}
@@ -103,16 +96,13 @@ public class SplashScreenFragment extends Fragment {
 			public void onClick(View arg) {
 				// Display the preferred connection dialog
 				if (!prefs.getBoolean("prefs_connection_dontAskAgain", false)) {
-					PreferredConnectionDialog rcFragment = PreferredConnectionDialog
-							.newInstance("Local Router");
-					rcFragment.show(getActivity().getSupportFragmentManager(),
-							"PreferredConnection");
+					PreferredConnectionDialog rcFragment = PreferredConnectionDialog.newInstance("Local Router");
+					rcFragment.show(getActivity().getSupportFragmentManager(),"PreferredConnection");
 				}
 
 				// Load the local login fragment
 				LocalLoginFragment localLoginFragment = new LocalLoginFragment();
-				FragmentTransaction transaction = getActivity()
-						.getSupportFragmentManager().beginTransaction();
+				FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
 				transaction.replace(R.id.login_fragment, localLoginFragment);
 				transaction.commit();
 			}
@@ -135,25 +125,10 @@ public class SplashScreenFragment extends Fragment {
 	// Animates the fragment
 	public void animate() {
 		zoomAnim = AnimationUtils.loadAnimation(getActivity(), R.anim.zoom_in);
-		fadeInAnim = AnimationUtils
-				.loadAnimation(getActivity(), R.anim.fade_in);
-		fadeInAndOutAnim = AnimationUtils.loadAnimation(getActivity(),
-				R.anim.fade_in_and_out);
-
-		// For the purpose of move animation (UP for ECM clouds, DOWN for Local
-		// city)
-		Display display = getActivity().getWindowManager().getDefaultDisplay();
-		final Point size = new Point();
-		display.getSize(size);
-		final int upOffset = (int) (0.25 * size.y);
-		final int downOffset = (int) (0.33 * size.y);
-		moveUp = new TranslateAnimation(ecm_image.getX(), ecm_image.getY(),
-				ecm_image.getX(), ecm_image.getY() - upOffset);
-		moveDown = new TranslateAnimation(local_image.getX(),
-				local_image.getY(), local_image.getX(), local_image.getY()
-						+ downOffset);
-		moveUp.setDuration(1000);
-		moveDown.setDuration(1000);
+		fadeInAnim = AnimationUtils.loadAnimation(getActivity(), R.anim.fade_in);
+		fadeInAndOutAnim = AnimationUtils.loadAnimation(getActivity(),R.anim.fade_in_and_out);
+		moveUp = AnimationUtils.loadAnimation(getActivity(), R.anim.move_up); 
+		moveDown = AnimationUtils.loadAnimation(getActivity(), R.anim.move_down); 
 
 		// This makes the animation permanent after initial rotation
 		if (hasAnimated) {
@@ -165,8 +140,8 @@ public class SplashScreenFragment extends Fragment {
 		}
 
 		// Create a set of animations for the two buttons
-		AnimationSet ecm_anim = new AnimationSet(false); // Zoom in and move up
-		AnimationSet local_anim = new AnimationSet(false); // Zoom in and move
+		final AnimationSet ecm_anim = new AnimationSet(false); // Zoom in and move up
+		final AnimationSet local_anim = new AnimationSet(false); // Zoom in and move
 															// down
 
 		// Makes effects of animation NOT reset
@@ -181,60 +156,72 @@ public class SplashScreenFragment extends Fragment {
 		local_anim.addAnimation(moveDown);
 
 		// Start animating the buttons
-		ecm_image.startAnimation(ecm_anim);
-		local_image.startAnimation(local_anim);
-
-		// Start animating the text
-		new Handler().postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				splash_text.startAnimation(fadeInAnim);
-			}
-		}, 1000);
-		
-		// Start animating the indicators
-		int interval = 1; // seconds
-		new Handler().postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				indicator[0].startAnimation(fadeInAndOutAnim);
-			}
-		}, interval * 2 * 1000);
-		
-		new Handler().postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				indicator[1].startAnimation(fadeInAndOutAnim);
-			}
-		}, interval * 3 * 1000);
-		
-		new Handler().postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				indicator[2].startAnimation(fadeInAndOutAnim);
-			}
-		}, interval * 4 * 1000);
-		
-		new Handler().postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				indicator[3].startAnimation(fadeInAndOutAnim);
-			}
-		}, interval * 2 * 1000);
-		
-		new Handler().postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				indicator[4].startAnimation(fadeInAndOutAnim);
-			}
-		}, interval * 3 * 1000);
-		
-		new Handler().postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				indicator[5].startAnimation(fadeInAndOutAnim);
-			}
-		}, interval * 4 * 1000);
+		if(!hasAnimated) {
+			new Handler().postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					ecm_image.startAnimation(ecm_anim);
+					local_image.startAnimation(local_anim);
+				}
+			}, 1000);
+	
+			// Start animating the text
+			new Handler().postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					splash_text.startAnimation(fadeInAnim);
+				}
+			}, 2000);
+			
+			// Start animating the indicators
+			int interval = 1; // seconds
+			new Handler().postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					indicator[0].startAnimation(fadeInAndOutAnim);
+				}
+			}, interval * 3 * 1000);
+			
+			new Handler().postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					indicator[1].startAnimation(fadeInAndOutAnim);
+				}
+			}, interval * 4 * 1000);
+			
+			new Handler().postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					indicator[2].startAnimation(fadeInAndOutAnim);
+				}
+			}, interval * 5 * 1000);
+			
+			new Handler().postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					indicator[3].startAnimation(fadeInAndOutAnim);
+				}
+			}, interval * 3 * 1000);
+			
+			new Handler().postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					indicator[4].startAnimation(fadeInAndOutAnim);
+				}
+			}, interval * 4 * 1000);
+			
+			new Handler().postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					indicator[5].startAnimation(fadeInAndOutAnim);
+				}
+			}, interval * 5 * 1000);
+		}
+		else {
+			ecm_image.startAnimation(ecm_anim);
+			local_image.startAnimation(local_anim);
+			splash_text.startAnimation(fadeInAnim);
+		}
 
 	}
 }
