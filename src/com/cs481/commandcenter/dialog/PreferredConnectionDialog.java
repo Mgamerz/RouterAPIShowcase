9ede00b5a2c6edffcb38543075dcd0232aeba233
Mgamerz <mgamerz2@gmail.com>
2014-03-06 15:44:45 -0700

    Fixed yes/no

--- src/com/cs481/commandcenter/dialog/PreferredConnectionDialog.java
+++ src/com/cs481/commandcenter/dialog/PreferredConnectionDialog.java
@@ -4,11 +4,9 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
-import android.content.res.Resources.Theme;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.DialogFragment;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CheckBox;
@@ -37,21 +35,17 @@ public class PreferredConnectionDialog extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         HoloDialogBuilder alertDialogBuilder = new HoloDialogBuilder(getActivity());
         alertDialogBuilder.setTitle(connectionType + " Connection");
-
-		Theme theme = getActivity().getTheme();
-		TypedValue typedValue = new TypedValue();
-		theme.resolveAttribute(android.R.attr.windowBackground, typedValue,
-				true);
-		// Color color = getResources().getColor(colorid);
-
-		alertDialogBuilder.setDividerColor(typedValue.resourceId);
-		alertDialogBuilder.setTitleColor(typedValue.resourceId);
+        final Resources resources = getResources();
+        
+        // THIS MAY NEED TO GET CHANGED (following two lines):
+        alertDialogBuilder.setTitleColor(resources.getString(R.color.CradlepointRed));
+        alertDialogBuilder.setDividerColor(resources.getString(R.color.CradlepointRed));
         
         LayoutInflater inflator = getActivity().getLayoutInflater();
         final View v = inflator.inflate(R.layout.dialog_preferredconnection, null);
         
         alertDialogBuilder.setCustomView(v);
-        alertDialogBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+        alertDialogBuilder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				//Check to see if the 'save profile' is selected.
@@ -69,7 +63,7 @@ public class PreferredConnectionDialog extends DialogFragment {
 			}
 		});
         
-        alertDialogBuilder.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+        alertDialogBuilder.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
