7541d521eedb17d92a24f93fe4b1631b25be1bb7
swright1021 <seanwright@u.boisestate.edu>
2014-03-11 10:00:15 -0600

    Started working on the .java and .xml client fragment.

---
+++ src/com/cs481/commandcenter/listrows/ClientListRow.java
@@ -0,0 +1,43 @@
+package com.cs481.commandcenter.listrows;
+
+import com.cs481.commandcenter.responses.status.lan.Client;
+
+public class ClientListRow {
+	private String title = "";
+	private String subtitle = "";
+	private Client client;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSubtitle() {
+		return subtitle;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public void setSubtitle(String subtitle) {
+		this.subtitle = subtitle;
+	}
+
+	public ClientListRow(String title) {
+		this.title = title;
+	}
+
+	public ClientListRow(Client client, String title, String subtitle) {
+		this.client = client;
+		this.title = title;
+		this.subtitle = subtitle;
+	}
+}
